f2e865a453e0d97d03bfb7c4b5a67a14210af3ca
Arksine <kode4food@yahoo.com>
2016-01-02 20:05:20 -0500

    Switched to clang from gcc

--- easycam/src/main/jni/Application.mk
+++ easycam/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
-NDK_TOOLCHAIN_VERSION := 4.9
+NDK_TOOLCHAIN_VERSION := clang
 APP_ABI := armeabi-v7a x86
 #  Enable C++11. However, pthread, rtti and exceptions arent enabled
 APP_CPPFLAGS += -std=c++11
